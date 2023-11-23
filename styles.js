66a99e439bd9d2f54534b9d0079fdb45ac8e047f
Vila71 <lolivila71@gmail.com>
2023-11-23 14:24:14 +0100

    cambios js

---
+++ styles.js
@@ -0,0 +1,9 @@
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".btn");
+
+botones.forEach(boton => {
+boton.addEventListener("click",()=>){
+
+}
+})
+
